dfdb7174518aa79a423ca525b56042374ee37247
Travis Gallegos <travis.gallegos@gmail.com>
2017-08-01 14:28:10 -0700

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert into the table burgers and the columns burger_name and if devoured (T or F)
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BLT', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cowboy', false);
